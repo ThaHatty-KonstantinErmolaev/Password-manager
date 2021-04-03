59d3d9c0a31cbdac113588cf013e2145c37a960c
Konstantin_Ermolaev <64197341+ThaHatty-KonstantinErmolaev@users.noreply.github.com>
2021-04-03 16:35:11 +0700

    Add files via upload

--- views/user/userpage.blade.php
+++ views/user/userpage.blade.php
@@ -10,7 +10,10 @@
     <div class="container">
         <div class="row justify-content-center">
 
+
         </div>
+
+
 
         <!--Информация пользователя-->
         <div class="row justify-content-center">
@@ -28,6 +31,9 @@
                 <span>Роль: {{ $user_item['role'] }}</span>
                 <br>
                 <button class="col-2 btn btn-outline-info"><a href="/laravel/password.local/public/profile/edit?id={{ $user_item['id'] }}" class="text-decoration-none text-info">Редактировать</a></button>
+                <div>
+                    <button class="col-2 btn btn-outline-danger"><a href="/laravel/password.local/public/profile/delete?id={{ $user_item['id'] }}" class="text-decoration-none text-danger">Удалить</a></button>
+                </div>
             @endforeach
         </div>
         <br>
@@ -35,48 +41,58 @@
         <!--Пароли пользователя-->
         <div class="container">
             <div class="row justify-content-between">
-        @foreach($user_passwords as $user_password)
+        @if($user_passwords == true)
+                    @foreach($user_passwords as $user_password)
 
-            <div class="card bg-dark border-white" style="width: 20rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $user_password['name'] }}</h5><span class="text-info">{{ $user_password['category'] }}</span>
-                    <h5 class="text-secondary"># {{ $user_password['tags'] }}</h5>
-                    <p class="card-text">{{ $user_password['description'] }}</p>
-                    <p class="card-text">Пароль: <span class="text-warning">{{ $user_password['value'] }}</span></p>
-                    <a href="#" class="btn btn-primary">Подробнее</a>
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Удалить
-                    </button>
-                    <!-- End button trigger -->
-                </div>
-            </div>
+                        <div class="card bg-dark border-white" style="width: 20rem;">
+                            <div class="card-body">
+                                <form action="/laravel/password.local/public/password/delete" method="get">
+                                    <h5 class="card-title">{{ $user_password['name'] }}</h5><span class="text-info">{{ $user_password['category'] }}</span>
+                                    <h5 class="text-secondary"># {{ $user_password['tags'] }}</h5>
+                                    <p class="card-text">{{ $user_password['description'] }}</p>
+                                    <p class="card-text">Пароль: <span class="text-warning">{{ $user_password['value'] }}</span></p>
+                                    <input type="hidden" value="{{ $user_password['id'] }}" id="id" name="id">
+                                    <div>
+                                        <a href="#" class="btn btn-primary">Подробнее</a>
+                                        <!-- Button trigger modal -->
+                                        <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            Удалить
+                                        </button>
+                                    </div>
+                                </form>
+                                <!-- End button trigger -->
+                            </div>
+                        </div>
 
-        @endforeach
+                    @endforeach
+        @endif
             </div>
         </div>
         <!---->
 
 
-        <!-- Modal -->
+        <!-- Modal
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
-                <div class="modal-content">
+                <div class="modal-content bg-dark">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title" id="exampleModalLabel">Удаление пароля</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <p class="">Вы точно хотите удалить этот пароль?</p>
+                        <p class="">{{}}</p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                        <button type="submit" class="btn btn-danger">Удалить</button>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- End modal -->
+         End modal -->
+
+
 
     </div>
 @endsection
